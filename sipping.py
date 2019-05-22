80f9d3562c17d43f6fa80916c40121d98db2db52
Laurie Odgers <laurie.odgers@fonedynamics.com.au>
2019-05-22 23:37:47 +1000

    fix LICENSE to replicate upstream randomly generate a source port if not specified extra comments

--- sipping.py
+++ sipping.py
@@ -3,16 +3,24 @@ import re
 import socket
 import sys
 import time
+import random
 
 class SipPing:
-    def __init__(self, dest_addr, dest_port=5060, src_ip='', src_port=55220, udp=True, timeout=1):
+    def __init__(self, dest_addr, dest_port=5060, src_ip='', src_port=None, udp=True, timeout=1):
         self.dest_addr = dest_addr
         self.dest_port = dest_port
         self.src_ip = src_ip
-        self.src_port = src_port
         self.udp = udp
         self.timeout = timeout
 
+        # generate a random port if src_port is None
+        if (src_port is None):
+            # randomly generate a port betewen 1024 and 65535
+            self.src_port = random.randint(1024, 65535)
+        else:
+            self.src_port = src_port
+
+        # set something that the sip options header can use as a src
         if (len(self.src_ip) > 0):
             sip_src_host = self.src_ip
         else:
@@ -35,6 +43,8 @@ Content-Length: 0
         # check formatting
         #print('"' + self.sip_options + '"')
 
+    # run a single ping with the options received in the constructor
+    # returns seconds as float
     def ping_once(self):
         # keep track of a timeout
         timeout = False
@@ -76,6 +86,7 @@ Content-Length: 0
 
         finally:
             endTime = time.time()
+
             try:
                 # Regardless of what happened, try to gracefully close down the
                 # socket.
@@ -92,6 +103,7 @@ Content-Length: 0
         return (endTime - startTime)
 
     # do many pings one after another
+    # returns list of seconds as list of float
     def ping(self, count, delay=0):
         results = []
 
@@ -99,12 +111,13 @@ Content-Length: 0
             # ping once
             results.append(self.ping_once())
 
+            # delay before next loop if we havent reached the end of the loop
             if (x < (count-1)):
                 time.sleep(delay)
 
         return results
 
-
+# examples
 #sipping = SipPing("10.1.2.3", 5060)
 
 # just 1 ping
